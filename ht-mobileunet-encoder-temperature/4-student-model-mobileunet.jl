7560dd3cd9bc1582985cd7e695feac543609b2a8
cirobr <ciro.rosa@alumni.usp.br>
2025-04-11 14:55:23 -0300

    add dropout values

--- ht-mobileunet-encoder-temperature/4-student-model-mobileunet.jl
+++ ht-mobileunet-encoder-temperature/4-student-model-mobileunet.jl
@@ -1,8 +1,8 @@
 # frontend student model
 function instantiate_model()
       Random.seed!(1234)   # to enforce reproducibility
-      drop_enc = (0.0, 0.2, 0.1, 0.1, 0.2)
-      drop_dec = ()
+      drop_enc = (0.0,0.2,0.1,0.1,0.2)
+      drop_dec = (0.1,0.0,0.2,0.0,0.2)
       return Chain(
             MobileUNet(3,C; drop_enc=drop_enc, drop_dec=drop_dec, verbose=false),
             x -> x[2][5],           # (20,20,1280,1)
